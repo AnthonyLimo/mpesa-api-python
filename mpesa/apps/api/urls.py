995fbcc49c19871239fb5256c9d593f4da232623
salmanyagaka <salmanyagaka@gmail.com>
2020-05-12 22:07:17 +0300

    Added an stk push

--- mpesa/apps/api/urls.py
+++ mpesa/apps/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('access_token', views.getAccessToken, name='get_mpesa_access_token'),
+    path('stk', views.lipa_na_mpesa_online, name='lipa_na_mpesa'),
 ]

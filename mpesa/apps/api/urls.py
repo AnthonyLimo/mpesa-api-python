79c9a2882ca164951180c4bbc8a2a388dc0c02de
salmanyagaka <salmanyagaka@gmail.com>
2020-05-13 15:03:39 +0300

    Added business to consumer functionality

--- mpesa/apps/api/urls.py
+++ mpesa/apps/api/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('simulate', views.simulate, name='simulate'),
     path('balance', views.balance, name='balance'),
 
+    path('b2c', views.bussiness_to_consumer, name='bussiness_to_consumer'),
+    path('b2c/timeout', views.b2c_timeout_url, name="b2c_timeout_url"),
+    path('b2c/result', views.b2c_result_url, name="b2c_result_url"),
+    # path('b2c/callback', views.call_back, name="call_back"),
+
+
     # register, confirmation, validation and callback urls
     path('c2b/register', views.register_urls, name="register_mpesa_validation"),
     path('c2b/confirmation', views.confirmation, name="confirmation"),

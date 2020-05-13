5ba77d7f94e77e52e64d8ac359c14d34010bd4c8
salmanyagaka <salmanyagaka@gmail.com>
2020-05-13 15:35:11 +0300

    Added reversal functionality

--- mpesa/apps/api/views.py
+++ mpesa/apps/api/views.py
@@ -108,6 +108,8 @@ def b2c_timeout_url(request):
     return JsonResponse(dict(context))
 
 
+
+
 @csrf_exempt
 def b2c_result_url(request):
     context = {
@@ -116,6 +118,43 @@ def b2c_result_url(request):
     }
     return JsonResponse(dict(context))
 
+def reversal(request):
+    access_token = MpesaAccessToken.validated_mpesa_access_token
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/reversal/v1/request"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    request = {
+        "Initiator": "apitest342",
+        "SecurityCredential": "OEnTXfEBwUauXnrDkVKdHP4QUJm8Q7stMX/lqsOhHC2sn+9NL7/vNaS0PBlGpbBEkmGc5XAhN+K0FLiHjYKhjOCGRKlFzuSRqyPvToSfysQUNge/rP3dinYe3IS3y7VpFWyOwSTbc0+hM+aeFB3RNM3pzMZGaYT5n5nsBNC6HsGuzryIWzoJDX2K8Qtb/xkCWfCfON0VPl6Zs+sq2nATRELK1vj6DwZ2wemDmQ2v1967MtFwu6F9spYS4IRoDo/XyYUWq+N74N7ZhvTHOhMxFww5JETfn/BPo1FuXPRXlGImi45FzFKYct7cpE2bjf9y1lPrmnv33FIf9JXoC4SAZQ==",
+        "CommandID": "TransactionReversal",
+        "TransactionID": "600744",
+        "Amount": "1000",
+        "ReceiverParty": "600744",
+        "RecieverIdentifierType": "4",
+        "ResultURL": "https://197.248.86.122:801/result_url",
+        "QueueTimeOutURL": "https://197.248.86.122:801/timeout_url",
+        "Remarks": "Thank you",
+        "Occasion": "DEC2019"
+    }
+    response = requests.post(api_url, json=request, headers=headers)
+    return HttpResponse(response.text)
+
+
+@csrf_exempt
+def timeout_url(request):
+    context = {
+        "ResultCode": 0,
+        "ResultDesc": "Timed out"
+    }
+    return JsonResponse(dict(context))
+
+
+@csrf_exempt
+def result_url(request):
+    context = {
+        "ResultCode": 0,
+        "ResultDesc": "Success"
+    }
+    return JsonResponse(dict(context))
 
 @csrf_exempt
 def register_urls(request):
